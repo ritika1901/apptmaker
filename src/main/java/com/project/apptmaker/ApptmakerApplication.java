a0ea3160599d925a13e6f3b4c8f6185c3edcf43d
Ritika <rrustagi@adobe.com>
2020-04-30 17:44:53 -0700

    initial commit

---
+++ src/main/java/com/project/apptmaker/ApptmakerApplication.java
@@ -0,0 +1,13 @@
+package com.project.apptmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptmakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApptmakerApplication.class, args);
+    }
+
+}
